4899b0730ea629061de55b3dcfefa6ab225a3587
Aaron Dewes <aaron.dewes@web.de>
2021-06-08 17:35:15 +0200

    Clean up the webpack config, downgrade read-pkg-up

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -18,7 +18,6 @@ const postcssPlugins = require( '@aarondewes/wp-postcss-plugins-preset' );
  * Internal dependencies
  */
 const {
-	getPackageProp,
 	hasBabelConfig,
 	hasPostCSSConfig,
 } = require( '../utils' );
@@ -55,32 +54,6 @@ const cssLoaders = [
 	},
 ];
 
-/**
- * Gets a unique identifier for the webpack build to avoid multiple webpack
- * runtimes to conflict when using globals.
- * This is polyfill and it is based on the default webpack 5 implementation.
- *
- * @see https://github.com/webpack/webpack/blob/bbb16e7af2eddba4cd77ca739904c2aa238a2b7b/lib/config/defaults.js#L374-L376
- *
- * @return {string} The generated identifier.
- */
-const getJsonpFunctionIdentifier = () => {
-	const jsonpFunction = 'webpackJsonp_';
-	const packageName = getPackageProp( 'name' );
-	if ( typeof packageName !== 'string' || ! packageName ) {
-		return jsonpFunction;
-	}
-	const IDENTIFIER_NAME_REPLACE_REGEX = /^([^a-zA-Z$_])/;
-	const IDENTIFIER_ALPHA_NUMERIC_NAME_REPLACE_REGEX = /[^a-zA-Z0-9$]+/g;
-
-	return (
-		jsonpFunction +
-		packageName
-			.replace( IDENTIFIER_NAME_REPLACE_REGEX, '_$1' )
-			.replace( IDENTIFIER_ALPHA_NUMERIC_NAME_REPLACE_REGEX, '_' )
-	);
-};
-
 const getLiveReloadPort = ( inputPort ) => {
 	const parsedPort = parseInt( inputPort, 10 );
 
@@ -95,10 +68,6 @@ const config = {
 	output: {
 		filename: '[name].js',
 		path: path.resolve( process.cwd(), 'build' ),
-		// Prevents conflicts when multiple webpack runtimes (from different apps)
-		// are used on the same page.
-		// @see https://github.com/WordPress/gutenberg/issues/23607
-		jsonpFunction: getJsonpFunctionIdentifier(),
 	},
 	resolve: {
 		alias: {
